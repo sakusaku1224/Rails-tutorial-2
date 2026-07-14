392717580ab09383f522482b714b98e01354235e
sakusaku1224 <sakusaku1224rako@gmail.com>
2026-07-14 22:51:13 +0000

    add: Microposts table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
+  # /users -> users#index
+  # /users/new -> users#new
+  # /users/:id -> users#show
+  # /users/:id/edit -> users#edit
+  # /users/:id -> users#update
+  # /users/:id -> users#destroy
   root "hello#index"
 end
